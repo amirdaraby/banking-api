6719d529ce0b77f340d3c6e9ab22382dab91c384
ArshiaMohammadei <a.mmdi2007m@gmail.com>
2025-05-24 12:55:32 +0330

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('api', [
-            \App\Http\Middleware\EnsureJsonResponse::class
+            \App\Http\Middleware\EnsureJsonResponse::class,
+            \App\Http\Middleware\ForceJsonResponse::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
