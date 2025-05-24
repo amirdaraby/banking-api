3676877c58143a91b8c846b81d19f33a97b5a62b
ArshiaMohammadei <a.mmdi2007m@gmail.com>
2025-05-24 12:54:15 +0330

    Create errors.php

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'model_not_found' => 'Resource not found',
+    'not_found' => 'Endpoint not found',
+    'method_not_allowed' => 'Method not allowed for this endpoint',
+    'too_many_requests' => 'Too many requests',
+    'server_error' => 'Internal server error',
+    'unauthorized' => 'Unauthorized access',
+    'forbidden' => 'Forbidden',
+    'validation_failed' => 'Validation failed',
+];
